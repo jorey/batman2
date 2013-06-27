4ab02f5c1debd7dca18a45426bda4e276e79dd6e
Jorey <joreymelinder@gmail.com>
2013-06-27 10:21:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Batman2::Application.config.secret_token = 'e03baed3fd87bb4fc70b42ae4211ac7f215fcb3a1c8b4fe2fd87fc31dee75f7eaf3486e3b1656a0380a2264ef67f0884d6d348bf9902c28621f1fc14a84f5101'
